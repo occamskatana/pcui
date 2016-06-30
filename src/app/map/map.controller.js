eaec9e4d1769ecfd184d3221cb1113a338ce66d8
john <johngallweycarter@gmail.com>
2016-06-30 16:53:47 -0400

    updates

--- src/app/map/map.controller.js
+++ src/app/map/map.controller.js
@@ -3,11 +3,19 @@
 	angular
 		.module('pcui')
 		.controller('MapController', function(NgMap){
+
+			$scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyD5hEVOsgZkdBXsBdF9t6psC5JuOxK2Agg";
+
+			
 			NgMap.getMap().then(function(map) {
 		    console.log(map.getCenter());
 		    console.log('markers', map.markers);
 		    console.log('shapes', map.shapes);
 		  });
+
+
+
+
 		});
 
 })();
