2a44327e8b062667854d0444b454b6ec174f52f8
john <johngallweycarter@gmail.com>
2016-08-25 20:22:05 -0400

    more work on the resident show view and avatar upgrade to the resident index view

--- src/app/resident-show/resident-controller.js
+++ src/app/resident-show/resident-controller.js
@@ -3,12 +3,28 @@
 		.module('pcui')
 		.controller('ResidentController', ResidentController)
 
-		function ResidentController($scope, $stateParams, ResidentInfoService){
+		function ResidentController($scope, $stateParams, ResidentInfoService, $firebaseArray){
 			$scope.residentInfo;
+			var ref = new Firebase('https://evolutiontech.firebaseio.com/residents/' +  $stateParams.id +'/tasks')
+
+			$scope.tasks = $firebaseArray(ref);
 
 			ResidentInfoService.getResidentInfo($stateParams.id).then(function(response){
 				$scope.residentInfo = response.data
-			})
+			});
+
+			$scope.toggleTaskComplete = function(task){
+				if (task.complete == false){
+					task.complete = true;
+				} else {
+					task.complete = false;
+				}
+
+				$scope.tasks.$save(task);
+			}
+
+
+
 
 			
 		};
